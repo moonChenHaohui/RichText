8933a21977498833275f086e3f503e747daed644
moon <237253995@qq.com>
2016-05-21 13:20:57 +0800

    init

---
+++ app/src/main/java/com/example/chenhaohui/richtext/Test.java
@@ -0,0 +1,10 @@
+package com.example.chenhaohui.richtext;
+
+/**
+ * Created by chenhaohui on 16/5/21.
+ */
+public class Test {
+    public static String getHello(){
+        return "hello";
+    }
+}
